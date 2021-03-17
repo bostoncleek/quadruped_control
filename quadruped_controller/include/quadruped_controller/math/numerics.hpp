d511fea54580379a16637983296b80be50260435
bostoncleek <boston@picknik.ai>
2021-03-16 20:55:32 -0600

    useful numerical utilities

---
+++ quadruped_controller/include/quadruped_controller/math/numerics.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file numerics.hpp
+ * @date 2021-03-12
+ * @author Boston Cleek
+ * @brief Numerical utility functions
+ */
+#ifndef NUMERICS_HPP
+#define NUMERICS_HPP
+
+namespace quadruped_controller
+{
+namespace math
+{
+constexpr double PI = 3.14159265358979323846;
+
+/**
+ * @brief approximately compare two floating-point numbers
+ * @param d1 - a number to compare
+ * @param d2 - a second number to compare
+ * @param epsilon - absolute threshold required for equality
+ * @return true if abs(d1 - d2) < epsilon
+ */
+bool almost_equal(double d1, double d2, double epsilon = 1.0e-12);
+}
+}
+#endif
---
+++ quadruped_controller/src/quadruped_controller/math/numerics.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file numerics.cpp
+ * @date 2021-03-12
+ * @author Boston Cleek
+ * @brief Numerical utility functions
+ */
+
+// C++
+#include <cmath>
+
+// Quadruped Control
+#include <quadruped_controller/math/numerics.hpp>
+
+
+namespace quadruped_controller
+{
+namespace math
+{
+bool almost_equal(double d1, double d2, double epsilon )
+{
+  return std::fabs(d1 - d2) < epsilon ? true : false;
+}
+
+
+}
+}
