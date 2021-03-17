fc9a680e9903ba31c0c8b70badf515f95e27d419
bostoncleek <boston@picknik.ai>
2021-03-16 20:58:40 -0600

    3D transform class

--- quadruped_controller/include/quadruped_controller/math/rigid3d.hpp
+++ quadruped_controller/include/quadruped_controller/math/rigid3d.hpp
@@ -14,37 +14,34 @@
 // Drake
 #include <drake/math/rotation_matrix.h>
 #include <drake/math/rigid_transform.h>
+#include <drake/math/roll_pitch_yaw.h>
+
 
 // Linear Algebra
 #include <armadillo>
 
 namespace quadruped_controller
 {
-namespace rigid3d
+namespace math
 {
 using std::tuple;
 
 using arma::mat;
 using arma::vec;
-
-// using drake::math::RotationMatrix;
-// using drake::math::RigidTransform;
-// // using drake::Quaternion;
-// using drake::Matrix3;
-
-// using Eigen::MatrixXd;
-// using Eigen::Map;
-// using Eigen::VectorXd;
-
-
-mat eigen_rotation_to_arma(const Eigen::MatrixXd& R_eigen);
-
-Eigen::MatrixXd arma_rotation_to_eigen(const mat& R_arma);
-
-mat skew_symmetric(const vec& x);
-
+using arma::vec3;
 
 class Rotation3d;
+
+mat eigen_to_arma(const Eigen::MatrixXd& R_eigen);
+
+// vec eigen_to_arma(const Eigen::VectorXd& p_eigen);
+
+Eigen::Matrix3d arma_rotation_to_eigen(const mat& R_arma);
+
+Eigen::Vector3d arma_translation_to_eigen(const vec3& p);
+
+mat skew_symmetric(const vec3& x);
+
 
 class Quaternion
 {
@@ -55,13 +52,23 @@ public:
 
   Quaternion(const Eigen::Quaterniond& q);
 
+  Quaternion(const Rotation3d& R);
+
+  Quaternion(const mat& R);
+
+  Quaternion(double angle, const vec3& axis);
+
   Rotation3d rotation() const;
 
-  mat rotationMatrix() const;
+  mat matrix() const;
+
+  vec3 eulerAngles() const;
 
   void print(const std::string& msg = "") const;
 
-  const drake::Quaternion<double>& data() const
+  bool isUnit() const;
+
+  const Eigen::Quaterniond& data() const
   {
     return q_;
   }
@@ -87,7 +94,7 @@ public:
   }
 
 private:
-  drake::Quaternion<double> q_;
+  Eigen::Quaterniond q_;
 };
 
 
@@ -98,15 +105,27 @@ public:
 
   Rotation3d(const Quaternion& quaternion);
 
-  Rotation3d(const mat& rotation_matrix);
+  Rotation3d(const mat& R);
+
+  Rotation3d(double roll, double pitch, double yaw);
+
+  vec3 operator*(const vec3& p) const
+  {
+    return matrix() * p;
+  }
 
   tuple<vec, double> angleAxis() const;
 
   vec angleAxisTotal() const;
 
-  mat rotationMatrix() const
+  mat matrix() const
   {
-    return eigen_rotation_to_arma(R_.matrix());
+    return eigen_to_arma(R_.matrix());
+  }
+
+  const drake::math::RotationMatrix<double>& data() const 
+  {
+    return R_;
   }
 
   Quaternion toQuaternion() const
@@ -114,14 +133,75 @@ public:
     return Quaternion(R_.ToQuaternion());
   }
 
+  Eigen::Quaterniond toEigenQuaternion() const
+  {
+    return R_.ToQuaternion();
+  }
+
   void print(const std::string& msg = "") const
   {
-    rotationMatrix().print(msg);
+    matrix().print(msg);
   }
 
 private:
   drake::math::RotationMatrix<double> R_;
 };
-}  // namespace rigid3d
+
+
+class Transform3d 
+{
+public:
+  Transform3d();
+
+  Transform3d(const vec3& p);
+
+  Transform3d(const drake::math::RigidTransform<double>& T);
+
+  Transform3d(const Quaternion& q, const vec3& p);
+
+  Transform3d(const Rotation3d& R, const vec3& p);
+
+  Transform3d(const mat& R, const vec3& p);
+
+  mat adjoint() const;
+
+  tuple<Quaternion, vec3> components() const;
+
+  Quaternion getQuaternion() const;
+
+  vec3 getTranslation() const;
+
+  Transform3d operator*(const Transform3d& T) const 
+  {
+    return Transform3d(T_ * T.data());
+  }
+
+  vec3 operator*(const vec3& p) const;
+  
+  const drake::math::RigidTransform<double>& data() const
+  {
+    return T_;
+  }
+
+  mat inverse() const 
+  {
+    return eigen_to_arma(T_.inverse().GetAsMatrix4());
+  }
+
+  mat matrix() const
+  {
+    return eigen_to_arma(T_.GetAsMatrix4());
+  }
+
+  void print(const std::string& msg = "") const
+  {
+    matrix().print(msg);
+  }
+
+private:
+  drake::math::RigidTransform<double> T_;
+
+};
+}  // namespace math
 }  // namespace quadruped_controller
 #endif
--- quadruped_controller/src/quadruped_controller/math/rigid3d.cpp
+++ quadruped_controller/src/quadruped_controller/math/rigid3d.cpp
@@ -5,30 +5,40 @@
  * @brief 3D rigid body lie algebra
  */
 
+// C++
 #include <iostream>
 #include <cmath>
+
+// Quadruped Control
 #include <quadruped_controller/math/rigid3d.hpp>
 
 namespace quadruped_controller
 {
-namespace rigid3d
+namespace math
 {
 using std::cos;
 using std::sin;
 
 
-mat eigen_rotation_to_arma(const Eigen::MatrixXd& R_eigen)
+mat eigen_to_arma(const Eigen::MatrixXd& R_eigen)
 {
   // Read only from R_eigen in memory
   // This is a copy operation for safety
   return mat(R_eigen.data(), R_eigen.rows(), R_eigen.cols());
 }
 
-Eigen::MatrixXd arma_rotation_to_eigen(const mat& R_arma)
+
+// vec eigen_to_arma(const Eigen::VectorXd& p_eigen)
+// {
+//   return mat(p_eigen.data(), p_eigen.rows(), p_eigen.cols());
+// }
+
+
+Eigen::Matrix3d arma_rotation_to_eigen(const mat& R_arma)
 {
   // Copy data over for safety
   // If Armadillo's memptr() is used this violates the arguements const
-  Eigen::MatrixXd R_eigen(3, 3);
+  Eigen::Matrix3d R_eigen;
   R_eigen(0, 0) = R_arma(0, 0);
   R_eigen(0, 1) = R_arma(0, 1);
   R_eigen(0, 2) = R_arma(0, 2);
@@ -45,7 +55,13 @@ Eigen::MatrixXd arma_rotation_to_eigen(const mat& R_arma)
 }
 
 
-mat skew_symmetric(const vec& x)
+Eigen::Vector3d arma_translation_to_eigen(const vec3& p)
+{
+  return Eigen::Vector3d(p(0), p(1), p(2));
+}
+
+
+mat skew_symmetric(const vec3& x)
 {
   mat skew(3, 3, arma::fill::zeros);
   // upper
@@ -63,19 +79,23 @@ mat skew_symmetric(const vec& x)
 
 /////////////////////////////////////////////////////////
 // Quaternion
-Quaternion::Quaternion() : q_(1., 0., 0., 0.)
-{
-}
+Quaternion::Quaternion() : q_(1., 0., 0., 0.) {}
 
 
-Quaternion::Quaternion(double qw, double qx, double qy, double qz) : q_(qw, qx, qy, qz)
-{
-}
+Quaternion::Quaternion(double qw, double qx, double qy, double qz) : q_(qw, qx, qy, qz) {}
 
 
-Quaternion::Quaternion(const Eigen::Quaterniond& q) : q_(q)
-{
-}
+Quaternion::Quaternion(const Eigen::Quaterniond& q) : q_(q) {}
+
+
+Quaternion::Quaternion(const Rotation3d& R) : q_(R.toEigenQuaternion()) {}
+
+
+Quaternion::Quaternion(const mat& R) : q_(Rotation3d(R).toEigenQuaternion()) {}
+
+
+Quaternion::Quaternion(double angle, const vec3& axis)
+ : q_(Eigen::AngleAxisd(angle, arma_translation_to_eigen(axis))) {}
 
 
 Rotation3d Quaternion::rotation() const
@@ -84,10 +104,24 @@ Rotation3d Quaternion::rotation() const
 }
 
 
-mat Quaternion::rotationMatrix() const
+mat Quaternion::matrix() const
 {
-  return Rotation3d(*this).rotationMatrix();
+  return Rotation3d(*this).matrix();
 }
+
+
+vec3 Quaternion::eulerAngles() const
+{
+  const drake::math::RollPitchYaw angles(q_);
+  return { angles.roll_angle(), angles.pitch_angle(), angles.yaw_angle() };
+}
+
+
+bool Quaternion::isUnit() const
+{
+  return std::sqrt(q_.x()*q_.x() + q_.y()*q_.y() + q_.z()*q_.z() + q_.w()*q_.w());
+}
+
 
 
 void Quaternion::print(const std::string& msg) const
@@ -103,20 +137,18 @@ void Quaternion::print(const std::string& msg) const
 
 /////////////////////////////////////////////////////////
 // Rotation
-Rotation3d::Rotation3d()
-{
-}
+Rotation3d::Rotation3d() {}
 
 
-Rotation3d::Rotation3d(const Quaternion& quaternion) : R_(quaternion.data())
-{
-}
+Rotation3d::Rotation3d(const Quaternion& quaternion) : R_(quaternion.data()) {}
 
 
-Rotation3d::Rotation3d(const mat& rotation_matrix)
-  : R_(arma_rotation_to_eigen(rotation_matrix))
-{
-}
+Rotation3d::Rotation3d(const mat& R)
+  : R_(arma_rotation_to_eigen(R)) {}
+
+
+Rotation3d::Rotation3d(double roll, double pitch, double yaw)
+  : R_(drake::math::RollPitchYaw<double>(roll, pitch, yaw)) {}
 
 
 tuple<vec, double> Rotation3d::angleAxis() const
@@ -137,9 +169,68 @@ vec Rotation3d::angleAxisTotal() const
 
 /////////////////////////////////////////////////////////
 // Transformation
+Transform3d::Transform3d() {}
 
 
-/////////////////////////////////////////////////////////
-// Adjoint
-}  // namespace rigid3d
+Transform3d::Transform3d(const vec3& p) : T_(arma_translation_to_eigen(p)) {}
+
+
+Transform3d::Transform3d(const drake::math::RigidTransform<double>& T) : T_(T) {}
+
+
+Transform3d::Transform3d(const Quaternion& q, const vec3& p)
+: T_(q.data(), arma_translation_to_eigen(p)) {}
+
+
+Transform3d::Transform3d(const Rotation3d& R, const vec3& p)
+: T_(R.data(), arma_translation_to_eigen(p)) {}
+
+
+Transform3d::Transform3d(const mat& R, const vec3& p)
+ : T_(Quaternion(R).data(), arma_translation_to_eigen(p)) {}
+
+
+mat Transform3d::adjoint() const
+{
+  // Assumes twist is [vy, vy, vz, wx, wy, wz]
+  mat AdT(6, 6, arma::fill::zeros);
+  const mat T = eigen_to_arma(T_.GetAsMatrix34());
+  const mat R_transpose = T.submat(0, 0, 2, 2).t();
+
+  AdT.submat(0, 0, 2, 2) = R_transpose;
+  AdT.submat(3, 3, 5, 5) = R_transpose;
+  AdT.submat(0, 3, 2, 5) = -1.0 * R_transpose * skew_symmetric(T.col(3));
+  
+  return AdT;
+}
+
+
+tuple<Quaternion, vec3> Transform3d::components() const
+{
+  const mat T = eigen_to_arma(T_.GetAsMatrix34());
+  return std::make_pair(Quaternion(T.submat(0, 0, 2, 2)), T.submat(0, 3, 2, 3));
+}
+
+
+Quaternion Transform3d::getQuaternion() const
+{
+  const mat T = eigen_to_arma(T_.GetAsMatrix34());
+  return Quaternion(T.submat(0, 0, 2, 2));
+}
+
+
+vec3 Transform3d::getTranslation() const
+{
+  const Eigen::Vector3d t_eigen = T_.translation();
+  return { t_eigen(0), t_eigen(1), t_eigen(2) };
+}
+
+
+vec3 Transform3d::operator*(const vec3& p) const
+{
+  const mat T = eigen_to_arma(T_.GetAsMatrix34());
+  // rotation followed by translation
+  return T.submat(0, 0, 2, 2) * p + T.col(3);
+} 
+}  // namespace math
 }  // namespace quadruped_controller
