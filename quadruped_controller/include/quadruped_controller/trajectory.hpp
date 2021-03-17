468a048e3292f65dec83460663d626b4fbf1761b
bostoncleek <boston@picknik.ai>
2021-03-16 20:56:08 -0600

    body and leg trajectory generator

---
+++ quadruped_controller/include/quadruped_controller/trajectory.hpp
@@ -0,0 +1,111 @@
+/**
+ * @file trajectory.hpp
+ * @date 2021-03-12
+ * @author Boston Cleek
+ * @brief Base and leg reference trajectory generators
+ */
+#ifndef TRAJECTORY_HPP
+#define TRAJECTORY_HPP
+
+// C++
+#include <cmath>
+
+// Linear Algebra
+#include <armadillo>
+
+// Quadruped Control
+#include <quadruped_controller/math/rigid3d.hpp>
+
+namespace quadruped_controller
+{
+using arma::mat;
+using arma::vec;
+using arma::vec3;
+
+using quadruped_controller::math::Quaternion;
+using quadruped_controller::math::Rotation3d;
+using quadruped_controller::math::Transform3d;
+
+struct Pose 
+{
+  Pose() : position(arma::fill::zeros) {}
+
+  Pose(const vec3& p): position(p) {}
+
+  Pose(const Quaternion& q): orientation(q) {}
+
+  Pose(const Transform3d& T)
+   : orientation(T.getQuaternion()), position(T.getTranslation())  {}
+
+  Pose(const Quaternion& q, const vec3& p)
+  : orientation(q), position(p) {}
+
+  Pose(const Rotation3d& R, const vec3& p)
+  : orientation(R), position(p) {}
+
+  Pose(const mat& R, const vec3& p)
+  : orientation(R), position(p) {}
+
+  Transform3d transform() const
+  {
+    return Transform3d(orientation, position);
+  }
+
+  void print(const std::string& msg = "") const
+  {
+    if (!msg.empty())
+    {
+      std::cout << msg << "\n";
+    }
+    orientation.print("orientation: ");
+    position.print("position: ");
+  }
+
+  Quaternion orientation;
+  vec3 position; // (x,y,z)
+};
+
+
+/**
+ * @brief Integrate a constant twist
+ * @param pose - position and orientaion of rigid body
+ * @param vb - body twist [vx, vy, vz, wx, wy, wz]
+ * @param dt - time step
+ * @return new pose
+ * @details Only considers [vx, vy, vz, wz] and sets the roll and pitch 
+ *  angles in the returned post to zero.
+ */
+Pose integrate_twist_yaw(const Pose& pose, const vec& u, double dt);
+
+
+/** @brief Base control (z, roll, pitch, yaw) only when standing 
+ */
+class StanceBaseControl 
+{
+public: 
+  StanceBaseControl();
+
+  StanceBaseControl(const Pose& pose);
+
+  void setPose(const Pose& pose);
+
+  Pose integrateTwist(const Pose& pose, const vec& u, double dt);
+
+private:
+  Pose pose_;
+  const unsigned int update{5};
+  unsigned int i{0};
+};
+
+
+/** @brief COM reference trajectory generator */ 
+class BaseTrajectory
+{
+public:
+  BaseTrajectory();
+
+private:
+
+};
+}  // namespace quadruped_controller
+#endif
---
+++ quadruped_controller/src/quadruped_controller/trajectory.cpp
@@ -0,0 +1,124 @@
+/**
+ * @file trajectory.cpp
+ * @date 2021-03-12
+ * @author Boston Cleek
+ * @brief Base and leg reference trajectory generators
+ */
+
+// Quadruped Control
+#include <quadruped_controller/trajectory.hpp>
+#include <quadruped_controller/math/numerics.hpp>
+
+namespace quadruped_controller
+{
+using std::cos;
+using std::sin;
+
+using math::Transform3d;
+
+Pose integrate_twist_yaw(const Pose& pose, const vec& u, double dt)
+{
+  // change in angle axis 
+  const vec3 delta_aa = u.rows(3, 5) * dt;
+  const double angle = arma::norm(delta_aa);
+
+  // rotation from b to b' 
+  mat Rbbp = arma::eye(3, 3);
+  // translation from b to b' 
+  vec3 tbbp(arma::fill::zeros);
+
+  // no rotation 
+  if (math::almost_equal(angle, 0.0))
+  {
+    tbbp = u.rows(0, 2) * dt;
+  }
+
+  else 
+  {
+    // construct rotation from change in anle axis 
+    const vec3 axis = delta_aa / angle;
+    Rbbp = Quaternion(angle, axis).matrix();
+
+    // rotate translation 
+    tbbp = Rbbp * u.rows(0, 2) * dt;
+  }
+
+  // Extract yaw 
+  const vec3 euler_angles = pose.transform().getQuaternion().eulerAngles();
+
+  // Twb' based on yaw only 
+  const Rotation3d Rwb_yaw(0.0, 0.0, euler_angles(2));
+  const Transform3d Twb_yaw(Rwb_yaw, pose.position);
+
+  // Twb' = Twb * Tbb' 
+  // const Transform3d Twbp = pose.transform() * Transform3d(Rbbp, tbbp);
+  const Transform3d Twbp = Twb_yaw * Transform3d(Rbbp, tbbp);
+
+  return Pose(Twbp);
+}
+
+
+/////////////////////////////////////////////////////////
+// StanceBaseControl
+StanceBaseControl::StanceBaseControl() {}
+
+
+StanceBaseControl::StanceBaseControl(const Pose& pose) : pose_(pose) {}
+
+
+void StanceBaseControl::setPose(const Pose& pose)
+{
+  pose_ = pose;
+}
+
+
+Pose StanceBaseControl::integrateTwist(const Pose& pose, const vec& u, double dt)
+{
+  // TODO update when error becomes too large
+  if (i == update)
+  {
+    pose_ = pose;
+  }
+
+  i++;
+
+  // change in angle axis 
+  const vec3 delta_aa = u.rows(3, 5) * dt;
+  const double angle = arma::norm(delta_aa);
+
+  // rotation from b to b' 
+  mat Rbbp = arma::eye(3, 3);
+  // translation from b to b' 
+  vec3 tbbp(arma::fill::zeros);
+
+  // no rotation 
+  if (math::almost_equal(angle, 0.0))
+  {
+    tbbp = u.rows(0, 2) * dt;
+  }
+
+  else 
+  {
+    // construct rotation from change in anle axis 
+    const vec3 axis = delta_aa / angle;
+    Rbbp = Quaternion(angle, axis).matrix();
+
+    // rotate translation 
+    tbbp = Rbbp * u.rows(0, 2) * dt;
+  }
+
+  // Twb' = Twb * Tbb' 
+  const Transform3d Twbp = pose_.transform() * Transform3d(Rbbp, tbbp);
+
+  // Update internal pose 
+  pose_ = Pose(Twbp);
+  return pose_;
+}
+
+
+/////////////////////////////////////////////////////////
+// BaseTrajectory
+BaseTrajectory::BaseTrajectory()
+{
+}
+}
